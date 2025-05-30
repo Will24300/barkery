4683bfe741119afa12774d0f0637dc871fe7f08c
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-05-30 16:54:46 +0200

    Add components & pages

--- barkery-cake/src/App.jsx
+++ barkery-cake/src/App.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
