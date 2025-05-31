23071fe6133b5ad00efff2cf2bba8e9d97958771
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-06-01 00:47:48 +0200

    Add bg1

--- barkery-cake/src/components/Products.jsx
+++ barkery-cake/src/components/Products.jsx
@@ -1,10 +1,11 @@
 import { products } from "../data/data";
 import bg from "../assets/productBg.png";
+import bg1 from "../assets/bg1.png";
 
 export default function Products() {
   return (
     <>
-      <section className="w-[85%] mx-auto text-center mb-10">
+      <section className="w-[85%] mx-auto text-center mb-30">
         <h2 className="font-sansita font-semibold text-[#111111] text-[35px] mb-15 mt-15">
           Top Products
         </h2>
@@ -17,7 +18,11 @@ export default function Products() {
               <img
                 src={product.img}
                 alt={product.name}
-                className="w-[170px] mx-auto"
+                className={
+                  product.id === 2 || product.id === 3
+                    ? "w-[200px] mx-auto mb-4"
+                    : "w-[170px] mx-auto"
+                }
               />
               <div className="flex flex-col  items-start">
                 <h2 className="font-medium text-[24px] mb-4">
@@ -27,7 +32,10 @@ export default function Products() {
                   <h2 className=" text-[22px] w-[70%] text-start">
                     {product.name}
                   </h2>
-                  <button className="bg-[#933C24] text-white px-4 py-1 rounded cursor-pointer">
+                  <button
+                    id="#explore"
+                    className="bg-[#933C24] text-white px-4 py-1 rounded cursor-pointer"
+                  >
                     Check
                   </button>
                 </div>
@@ -36,6 +44,20 @@ export default function Products() {
           ))}
         </div>
       </section>
+      <div
+        className="h-[60vh] bg-center bg-cover mb-20 flex flex-col justify-center items-center"
+        style={{ backgroundImage: `url(${bg1})` }}
+      >
+        <h2 className="text-[40px] font-sansita text-[#933C24] w-[25%] text-center leading-13">
+          20% Off&nbsp; Your First Order
+        </h2>
+        <p className="text-[#5D5D5D] text-[17px] w-[25%] text-center my-5">
+          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibenjgg.
+        </p>
+        <button className="bg-[#933C24] text-white font-smibold py-2 px-8 rounded cursor-pointer">
+          Learn More
+        </button>
+      </div>
     </>
   );
 }
