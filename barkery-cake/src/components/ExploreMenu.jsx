6ff0cda02b542666d0f2d1673c6c67f1c501d545
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-06-01 13:24:56 +0200

    add change on add button

--- barkery-cake/src/components/ExploreMenu.jsx
+++ barkery-cake/src/components/ExploreMenu.jsx
@@ -4,8 +4,15 @@ import { data } from "../data/data";
 export default function ExploreMenu() {
   const items = ["Cake", "Muffins", "Croissant", "Bread", "Tart", "Favorite"];
   const [isActiveIndex, setIsActiveIndex] = useState(0);
-  const [counter, setCounter] = useState(0);
+  const [counters, setCounters] = useState({});
   const categorie = data.categories[isActiveIndex].products;
+
+  const updateCounter = (id) => {
+    setCounters((prev) => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
+  };
+  const updateCounterDecrease = (id) => {
+    setCounters((prev) => ({ ...prev, [id]: prev[id] - 1 }));
+  };
 
   const handleToggle = (index) => {
     setIsActiveIndex(isActiveIndex === index ? 0 : index);
@@ -36,23 +43,32 @@ export default function ExploreMenu() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 place-items-center">
           {categorie.map((item, index) => (
-            <div className="w-[250px] h-[350px] rounded-t flex flex-col shadow relative">
+            <div
+              className="w-[250px] h-[350px] rounded-t flex flex-col shadow relative"
+              key={index}
+            >
               <div
                 className="h-4/5 bg-center bg-cover rounded-t  "
                 style={{ backgroundImage: `url(${item.image})` }}
               ></div>
               <div
                 className={
-                  counter !== 0
-                    ? "w-[80px] rounded-2xl bg-white absolute top-[48%] left-2  flex justify-between items-center "
+                  counters[item.id] > 0
+                    ? "w-[80px] rounded-2xl bg-white absolute top-[45%] left-2 p-1  flex justify-between items-center "
                     : "hidden"
                 }
               >
-                <span className="bg-red-300 text-red-600 text-[16px] px-2 rounded-2xl cursor-pointer">
+                <span
+                  onClick={() => updateCounterDecrease(item.id)}
+                  className="bg-red-300 text-red-600 text-[16px] px-2 rounded-2xl cursor-pointer"
+                >
                   -
                 </span>
-                <span>1</span>
-                <span className="bg-green-200 text-green-800 px-2 rounded-2xl cursor-pointer">
+                <span className="px-1">{counters[item.id] || 0}</span>
+                <span
+                  onClick={() => updateCounter(item.id)}
+                  className="bg-green-200 text-green-800 px-2 rounded-2xl cursor-pointer"
+                >
                   +
                 </span>
               </div>
@@ -64,10 +80,10 @@ export default function ExploreMenu() {
                 <div className="flex justify-between items-center">
                   <p className="text-[#933C24] font-semibold">${item.price}</p>
                   <button
-                    className="bg-[#933C24] text-white py-1 px-6 cursor-pointer rounded"
-                    onClick={() => setCounter((prev) => prev + 1)}
+                    className="block bg-[#933C24] text-white py-1 px-6 cursor-pointer rounded"
+                    onClick={() => updateCounter(item.id)}
                   >
-                    Add
+                    {counters[item.id] === 0 ? "Add " : counters[item.id]}
                   </button>
                 </div>
               </div>
