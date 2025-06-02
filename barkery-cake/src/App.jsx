1850a6e05744e1a4d60d517064f7ee152e6feba6
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-06-02 06:42:13 -1200

    Add login and signup pages with authentication flow

--- barkery-cake/src/App.jsx
+++ barkery-cake/src/App.jsx
@@ -2,8 +2,6 @@ import { useState, useEffect, useContext } from "react";
 import Home from "./pages/Home";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
-import AdminForm from "./pages/AdminForm";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Mainpage from "./admin/Mainpage";
