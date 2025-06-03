cb06acfdc89dad5c62f16179662ddf038ae4b3b0
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-06-03 09:53:45 -1200

    Added api config

--- barkery-cake/vite.config.js
+++ barkery-cake/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:8082/api",
+        target: "http://localhost:8082",
         changeOrigin: true,
       },
     },
