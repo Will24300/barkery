3f145c9be72e6636cd2aefc8c6fdbdcbd603a246
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-06-03 09:14:42 -1200

    Removed -user from route

--- barkery-cake/src/pages/Signup.jsx
+++ barkery-cake/src/pages/Signup.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("/auth/register-user", formData);
+      const response = await axios.post("/auth/register", formData);
       // Notify on success
       toast.success(response.data.message || "Registration successful!");
 
